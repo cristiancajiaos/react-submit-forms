f94425e99b0178219ec28e11405f7751addc4e81
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-09 13:12:28 -0400

    Creado formulario con submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("John")
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(`Hi. My name is ${name}.`);
+  }
 
   return (
     <>
       <section className="container my-2">
-        <p>Layout</p>
+        <form onSubmit={handleSubmit}>
+          <label>Enter your name</label>
+          <input value={name} onChange={handleChange} className="form-control mb-2"/>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
+        <hr/>
+
+        <p>Current value: {name}</p>
       </section>
     </>
   )
